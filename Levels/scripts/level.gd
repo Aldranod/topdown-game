b567361bbd385ccfecdc545727d37900bd39aa5d
Aldranod <kurzydlo.b@gmail.com>
2025-12-18 11:30:27 +0100

    minor adjustments

--- Levels/scripts/level.gd
+++ Levels/scripts/level.gd
@@ -3,24 +3,40 @@ class_name  Level extends Node2D
 @export var music : AudioStream
 @export var cutscene : bool = false
 @export var cutscene_trigger : NPC
-@export var target_node : Node
 @export var darkness : bool = false
-
+@export var weather : bool = false
 
 func _ready() -> void:
 	self.y_sort_enabled = true
 	PlayerManager.set_as_parent( self )
 	LevelManager.level_load_started.connect( _free_level)
 	AudioManager.play_music(music)
+	play_cutscene()
+	darkness_check()
+	weatcher_check()
+		
+func play_cutscene() -> void:
 	if cutscene:
-		play_cutscene()
+		if cutscene_trigger:
+			for c in cutscene_trigger.get_children():
+				if c is DialogInteraction:
+					c.player_interact()	
+	else:
+		return
+
+func darkness_check() -> void:
 	if darkness:
 		PlayerManager.player.player_light_switch(true)
 	else:
 		PlayerManager.player.player_light_switch(false)
-	if $WeatherManager:
-		$WeatherManager.change_to(Drizzle)	
+	pass
+	
+func weatcher_check() -> void:
+	if weather:
+		$WeatherManager.change_to(HeavyRain)	
 		$WeatherManager.change_to(Fog)	
+	else:
+		return	
 
 func _free_level() -> void:
 	PlayerManager.unparent_player( self )
@@ -29,15 +45,6 @@ func _free_level() -> void:
 func player_camera_switch() -> void:
 	if PlayerManager.player:
 		PlayerManager.player.player_camera_switch()
-
-func play_cutscene() -> void:
-	#cutscene_camera_switch()
-	if cutscene_trigger:
-		for c in cutscene_trigger.get_children():
-			if c is DialogInteraction:
-				
-				c.player_interact()		
-	pass
 
 func cutscene_camera_switch() -> void:
 	if $Camera2D:
@@ -49,7 +56,7 @@ func end_cutscene() -> void:
 	#$LevelTransition._player_entered(PlayerManager.player)
 	pass
 
-func kill_node( target_node) -> void:
+func kill_node(target_node : Node) -> void:
 	target_node.queue_free()
 	pass
 
