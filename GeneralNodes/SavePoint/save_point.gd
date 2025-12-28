6c08cefb81cf439e38395279c0ddfe83309de38a
Aldranod <kurzydlo.b@gmail.com>
2025-12-28 13:44:09 +0100

    1st q cutscene

--- GeneralNodes/SavePoint/save_point.gd
+++ GeneralNodes/SavePoint/save_point.gd
@@ -25,16 +25,16 @@ func _on_area_exit( _a : Area2D) -> void:
 func player_interact() -> void:
 	bonfire_used.emit()
 	label_animation_player.play("saved")
-	gpu_particles_2d.amount = 50
-	gpu_particles_2d.explosiveness = 1
-	particles.initial_velocity_min = 50
-	particles.initial_velocity_max = 100
+	#gpu_particles_2d.amount = 50
+	#gpu_particles_2d.explosiveness = 1
+	#particles.initial_velocity_min = 50
+	#particles.initial_velocity_max = 100
 	gpu_particles_2d.emitting = true
 	await get_tree().create_timer(0.5).timeout
-	gpu_particles_2d.amount = 10
-	gpu_particles_2d.explosiveness = 1
-	particles.initial_velocity_min = 10
-	particles.initial_velocity_max = 30
+	#gpu_particles_2d.amount = 50
+	#gpu_particles_2d.explosiveness = 1
+	#particles.initial_velocity_min = 10
+	#particles.initial_velocity_max = 30
 	SaveManager.save_game()
 	pass	
 		
